054649ef079a6007854dd1a1a062659742b85e3a
Juan Edi <jedi@manas.com.ar>
2017-05-19 16:47:21 -0300

    Setup Webpack to strip flow annotations

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015']
+            presets: ['es2015', 'flow']
           }
         }
       },
