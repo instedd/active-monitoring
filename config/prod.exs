f021cb97f4e51166c8e66f0bac69b78a129f8a82
Santiago Palladino <spalladino@gmail.com>
2017-06-13 11:40:01 -0300

    Use SSL on production

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :active_monitoring, ActiveMonitoring.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: {:system, "HOST"}, scheme: "https", port: 443],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
