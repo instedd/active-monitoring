0ecbc392aca38aecbda4469dd7bb3682a16f6709
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2017-06-27 12:47:22 -0300

    Lint fixes

--- web/static/js/selectors/campaign.js
+++ web/static/js/selectors/campaign.js
@@ -1,12 +1,12 @@
 export const audioEntries = (state) => {
   const langs = state.campaign.data.langs || []
   const symptoms = (state.campaign.data.symptoms || []).map(([id, name]) => `symptom:${id}`)
-  let topics = ['welcome'].concat(symptoms).concat(['forward', 'additional_information_intro','educational', 'thanks'])
-  if(state.campaign.data.additionalInformation == 'zero' || state.campaign.data.additionalInformation == undefined) {
+  let topics = ['welcome'].concat(symptoms).concat(['forward', 'additional_information_intro', 'educational', 'thanks'])
+  if (state.campaign.data.additionalInformation == 'zero' || state.campaign.data.additionalInformation == undefined) {
     const i = topics.indexOf('additional_information_intro')
     topics.splice(i, 2)
   }
-  if(state.campaign.data.additionalInformation == 'compulsory') {
+  if (state.campaign.data.additionalInformation == 'compulsory') {
     const i = topics.indexOf('additional_information_intro')
     topics.splice(i, 1)
   }
