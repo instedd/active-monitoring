e05066182d4a8f3194f8093c2a8b528fee81eece
Paula Mallol <pmallol@manas.com.ar>
2017-08-08 18:21:13 -0300

    Style and fix Campaign overview

--- web/static/js/components/campaigns/CampaignDashboard.jsx
+++ web/static/js/components/campaigns/CampaignDashboard.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Paper from 'react-md/lib/Papers'
+import List from 'react-md/lib/Lists/List';
+import ListItem from 'react-md/lib/Lists/ListItem';
+import FontIcon from 'react-md/lib/FontIcons';
+import Divider from 'react-md/lib/Dividers'
 import 'react-vis/dist/styles/legends.scss'
 import {
   XYPlot,
@@ -28,82 +32,115 @@ export default class CampaignDashboard extends Component {
       weeksInChart.push(currentWeek - i)
     }
     return (
-      <div>
+      <div className='md-container'>
         <div className='md-grid'>
           <div className='md-cell md-cell--12'>
             <h3>Campaign performance</h3>
           </div>
         </div>
         <div className='md-grid'>
-          <div className='md-cell md-cell--2'>
+          <div className='md-cell md-cell--3 dashboard-data'>
             <div className='md-grid'>
-              <div className='md-cell red'>{this.props.subject_stats.cases} Cases detected</div>
-              <div className='md-cell'>{this.props.subject_stats.totalSubjects} Enrolled callers</div>
+              <List className='md-cell md-cell--8'>
+                <ListItem
+                  rightIcon={<FontIcon>keyboard_arrow_right</FontIcon>}
+                  primaryText={this.props.subject_stats.cases}
+                  secondaryText="Cases detected"
+                  className="red-text"
+                />
+                <ListItem
+                  rightIcon={<FontIcon>keyboard_arrow_right</FontIcon>}
+                  primaryText={this.props.subject_stats.totalSubjects}
+                  secondaryText="Enrolled callers"
+                />
+                </List>
             </div>
           </div>
-          <div className='md-cell md-cell--10'>
-            <Paper zDepth={2} className='white'>
-              <h2>Weekly enrolled subjects</h2>
-              <XYPlot
-                xType='ordinal'
-                width={600}
-                height={400}
-                xDistance={100}
-                stackBy='y'>
-                <HorizontalGridLines
-                  style={{'stroke': '#E4E4E4'}}
-                  left={12} />
-                <XAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}} />
-                <YAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}}
-                  top={20} />
-                <LineSeries
-                  data={this.props.subject_stats.timeline}
-                  color='#4FAF54' />
-              </XYPlot>
+          <div className='md-cell md-cell--9 dashboard-chart'>
+            <Paper zDepth={2} className='white rounded-corners'>
+              <div className='md-grid'>  
+                <div className='md-cell md-cell--12'>
+                  <h2>Weekly enrolled subjects</h2>
+                  <XYPlot
+                    xType='ordinal'
+                    width={600}
+                    height={400}
+                    xDistance={100}
+                    stackBy='y'>
+                    <HorizontalGridLines
+                      style={{'stroke': '#E4E4E4'}}
+                      left={12} />
+                    <XAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}} />
+                    <YAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}}
+                      top={20} />
+                    <LineSeries
+                      data={this.props.subject_stats.timeline}
+                      color='#4FAF54' />
+                  </XYPlot>
+                </div>
+              </div>
             </Paper>
           </div>
         </div>
-
+        <Divider className='dashboard-divider'/>
         <div className='md-grid'>
           <div className='md-cell md-cell--12'>
             <h3>Calls performance</h3>
           </div>
         </div>
         <div className='md-grid'>
-          <div className='md-cell md-cell--2'>
+          <div className='md-cell md-cell--3 dashboard-data'>
             <div className='md-grid'>
-              <div className='md-cell'>{this.props.call_stats.today} Calls today</div>
-              <div className='md-cell'>{this.props.call_stats.lastWeek} Calls in the last 7 days</div>
-              <div className='md-cell'>{this.props.call_stats.successfulOverall} Successful calls</div>
+              <List className='md-cell md-cell--8'>
+                <ListItem
+                  rightIcon={<FontIcon>keyboard_arrow_right</FontIcon>}
+                  primaryText={this.props.call_stats.today}
+                  secondaryText="Calls today"
+                />
+                <ListItem
+                  rightIcon={<FontIcon>keyboard_arrow_right</FontIcon>}
+                  primaryText={this.props.call_stats.lastWeek}
+                  secondaryText="Calls last 7 days"
+                />
+                <ListItem
+                  rightIcon={<FontIcon>keyboard_arrow_right</FontIcon>}
+                  primaryText={this.props.call_stats.successfulOverall}
+                  secondaryText="Successful calls"
+                />
+              </List>
             </div>
           </div>
-          <div className='md-cell md-cell--10'>
-            <Paper zDepth={2} className='white'>
-              <h2>Weekly calls by status</h2>
-              <DiscreteColorLegend
-                orientation='horizontal'
-                items={[{title: 'Successful', color: '#4FAF54'}, {title: 'Partial', color: '#FEC12E'}]} />
-              <XYPlot
-                xType='ordinal'
-                width={600}
-                height={400}
-                xDistance={100}
-                stackBy='y'>
-                <HorizontalGridLines
-                  style={{'stroke': '#E4E4E4'}}
-                  left={12} />
-                <XAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}} />
-                <YAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}}
-                  top={20} />
-                <VerticalBarSeries
-                  className='success'
-                  data={this.props.call_stats.timeline[0]}
-                  color='#4FAF54' />
-                <VerticalBarSeries
-                  className='partial'
-                  data={this.props.call_stats.timeline[0]}
-                  color='#FEC12E' />
-              </XYPlot>
+          <div className='md-cell md-cell--9 dashboard-chart'>
+            <Paper zDepth={2} className='white rounded-corners'>
+              <div className='md-grid'>  
+                <div className='md-cell md-cell--12'>
+                  <h2>Weekly calls by status</h2>
+                  <DiscreteColorLegend
+                    orientation='horizontal'
+                    items={[{title: 'Successful', color: '#4FAF54'}, {title: 'Partial', color: '#FEC12E'}]} />
+                  <XYPlot
+                    xType='ordinal'
+                    width={600}
+                    height={400}
+                    xDistance={100}
+                    stackBy='y'>
+                    <HorizontalGridLines
+                      style={{'stroke': '#E4E4E4'}}
+                      left={12} />
+                    <XAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}} />
+                    <YAxis style={{text: {stroke: 'none', fill: '#A4A4A4', fontSize: 12, top: 4}}}
+                      top={20} />
+                    <VerticalBarSeries
+                      className='success'
+                      data={this.props.call_stats.timeline[0]}
+                      color='#4FAF54' />
+                    <VerticalBarSeries
+                      className='partial'
+                      data={this.props.call_stats.timeline[0]}
+                      color='#FEC12E' />
+                  </XYPlot>
+                </div>
+              </div>
             </Paper>
           </div>
         </div>
