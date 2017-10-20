90a8d31e20d8a645f1b2fc509f8e291d005ca97d
Matias Garcia Isaia <mgarcia@manas.tech>
2017-10-20 15:40:08 -0300

    Fix navigation buttons in Campaign creation form

--- web/static/js/components/ScrollToLink.jsx
+++ web/static/js/components/ScrollToLink.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Button from 'react-md/lib/Buttons/Button'
+import $ from 'jquery'
 
 export class ScrollToLink extends Component {
   render() {
@@ -22,11 +23,7 @@ ScrollToLink.propTypes = {
 
 export const animatedScrollTo = (e: Event, target: string) => {
   e.preventDefault()
-
-  // $('html, body').animate({
-  //   scrollTop: $(target).offset().top
-  // }, 500)
-  var el = document.getElementById(target)
-  var rect = el.getBoundingClientRect()
-  window.scrollTo(0, rect.top)
+  $('html, body').animate({
+    scrollTop: $(`#${target}`).offset().top
+  }, 500)
 }
