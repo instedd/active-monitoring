2a60ab5e86b7ddbc8c74ae1963a06925162959e3
Matias Garcia Isaia <mgarcia@manas.tech>
2017-11-13 20:08:10 -0300

    Delete outdated tests
    They have already been implemented in this file or in verboice_callbacks_controller_test

--- test/controllers/campaigns_controller_test.exs
+++ test/controllers/campaigns_controller_test.exs
@@ -83,35 +83,4 @@ defmodule ActiveMonitoring.CampaignsControllerTest do
     campaign = build(:campaign, user: user) |> Repo.insert!
     [campaign: campaign]
   end
-
-  # describe "index honors permissions" do
-  #   test "index doesn't list another user's campaigns" do
-  #     response = build_conn()
-  #     |> get(campaigns_path(build_conn(), :index))
-  #     |> json_response(200)
-
-  #   end
-  # end
-
-  # describe "on call receive" do
-    # setup do
-    #   owner = build(:user, email: "test@example.com") |> Repo.insert!
-    #   other = build(:user, email: "other@example.com") |> Repo.insert!
-    #   campaign = build(:campaign, user: other) |> Repo.insert!
-    #   {:ok, campaign: campaign}
-    # end
-
-  #   test "answers a verboice status call", %{conn: conn, campaign: campaign} do
-  #     cs = Campaign.changeset(campaign, %{})
-  #     Repo.update(Ecto.Changeset.put_change(cs, :started_at, Ecto.DateTime.utc()))
-  #     conn = post(conn, verboice_callbacks_path(conn, :callback, campaign.id, CallSid: "abc123"))
-  #     assert conn.status == 200
-  #   end
-
-  #   test "refuses a call if campaign hasn't begun", %{conn: conn, campaign: campaign} do
-  #     conn = post conn, verboice_callbacks_path(conn, :callback, campaign.id, CallSid: "abc123")
-  #     assert conn.resp_body == "<Hangup/>"
-  #     assert conn.status == 503
-  #   end
-  # end
 end
