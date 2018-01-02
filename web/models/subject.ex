7b382e030dcb85c72e4c92150934d9cbf3fd755e
Matias Garcia Isaia <mgarcia@manas.tech>
2018-01-02 15:45:37 -0300

    Scope call stats by campaign
    Fixes #69
    See #67

--- web/models/subject.ex
+++ web/models/subject.ex
@@ -82,7 +82,7 @@ defmodule ActiveMonitoring.Subject do
           calls_with_at_least_one = Repo.all(from c in Call,
             join: ca in CallAnswer, on: ca.call_id == c.id,
             join: s in Subject, on: c.subject_id == s.id,
-            where: ca.response == ^true, select: [c.id, s.id])
+            where: ca.response == ^true and c.campaign_id == ^campaign_id, select: [c.id, s.id])
           chunked = Enum.chunk_by(calls_with_at_least_one, fn([c,_]) -> c end)
           both_yes = Enum.filter(chunked, fn(l) -> Enum.count(l) > 1 end)
           subjects = Enum.map(both_yes, fn([[_,a],_]) -> a end)
@@ -90,10 +90,10 @@ defmodule ActiveMonitoring.Subject do
         _ ->
           Repo.one(from c in Call,
             join: ca in CallAnswer, on: ca.call_id == c.id,
-            where: ca.response == ^true, select: count("id"))
+            where: ca.response == ^true and c.campaign_id == ^campaign_id, select: count("id"))
       end
     total_subjects = Repo.one(from s in Subject, select: count("id"), where: s.campaign_id == ^campaign_id)
-    timeline = Repo.all((from s in Subject, where: s.inserted_at >= ^subtract(now(), Timex.Duration.from_days(90))) |> Call.by_week(:inserted_at))
+    timeline = Repo.all((from s in Subject, where: s.campaign_id == ^campaign_id and s.inserted_at >= ^subtract(now(), Timex.Duration.from_days(90))) |> Call.by_week(:inserted_at))
     %{cases: cases, total_subjects: total_subjects, timeline: timeline}
   end
 end
