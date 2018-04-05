e83de4d3e1272c9ce3d6e54330e404594080d4b5
Martin Verzilli <mverzilli@manas.tech>
2018-04-05 16:53:48 -0300

    Set webpack to watch and generate files instead of running as web server

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -42,19 +42,10 @@ defmodule ActiveMonitoring.LayoutView do
   end
 
   def js_script_tag do
-    if Mix.env == :prod do
-      "<script src=\"/js/app.js\"></script>"
-    else
-      "<script src=\"http://localhost:4001/js/app.js\"></script>"
-    end
+    "<script src=\"/js/app.js\"></script>"
   end
 
   def css_script_tag do
-    if Mix.env == :prod do
-      "<link rel=\"stylesheet\" href=\"/css/app.css\">"
-    else
-      "<link rel=\"stylesheet\" href=\"http://localhost:4001/css/app.css\">"
-    end
+    "<link rel=\"stylesheet\" href=\"/css/app.css\">"
   end
-
 end
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,40 @@
 var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
-var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin')
 
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-    host: '0.0.0.0',
-    port: 4001,
-    overlay: true
-  },
-
   resolve: {
-
-    /* Load path for required files */
     modules: [
-      path.join(__dirname, 'node_modules'),
-      path.join(__dirname, '_web/static/js')
+      'node_modules',
+      path.join(__dirname, '/web/static/js')
     ],
-
     extensions: ['.js', '.jsx']
   },
-
-  /*
-   Entry points.
-   Only code referenced from this files will be included in the target bundles.
-   */
-  entry: [
-    './web/static/js/app.jsx',
-    './web/static/css/app.scss'
-  ],
+  entry: {
+    app: [
+      './web/static/js/app.jsx',
+      './web/static/css/app.scss'
+    ]
+  },
 
   output: {
-    path: path.join(__dirname, 'priv/static'),
-    filename: 'js/app.js'
+    path: path.join(__dirname, '/priv/static'),
+    filename: 'js/[name].js'
   },
 
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /static\/vendor/
+        ],
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015', 'stage-0', 'react'],
-            plugins: ['transform-object-rest-spread', 'transform-flow-comments']
+            presets: ['es2015', 'react', 'stage-0', 'flow'],
+            plugins: ['transform-object-rest-spread']
           }
         }
       },
@@ -55,25 +44,33 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({use: [{loader: 'css-loader'}, {loader: 'sass-loader'}]})
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: false
+              }
+            },
+            {
+              loader: 'sass-loader?sourceMap',
+              options: {
+                includePaths: [
+                  path.join(__dirname, '/web/static/vendor/css'),
+                  path.join(__dirname, '/node_modules')
+                ]
+              }
+            }
+          ]
+        })
       }
     ]
   },
 
-  /* Include sourcemaps along generated files */
   devtool: 'cheap-module-source-map',
 
   plugins: [
-    /* Extract all compiled styles into a separate stylesheet instead of
-       requiring CSS from javascript files as Webpack does by default. */
-    new ExtractTextPlugin('css/app.css'),
-
-    /* Copy static assets from web/static/assets.
-       Note that when new messages are added we still need to restart the webpack
-       watch.
-     */
-    new CopyWebpackPlugin([{ from: './web/static/assets' }]),
-
-    new FlowBabelWebpackPlugin()
+    new ExtractTextPlugin('css/[name].css'),
+    new CopyWebpackPlugin([{ from: './web/static/assets' }])
   ]
 }
